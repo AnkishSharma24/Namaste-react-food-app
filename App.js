3249e5a9c789e076b60d502ecf19f68a2359c522
AnkishSharma24 <142089148+AnkishSharma24@users.noreply.github.com>
2024-05-31 13:53:32 +0530

    Header Complete

--- App.js
+++ App.js
@@ -5,18 +5,54 @@ import React from "react";
 
 const Header = ()=>{
     return(
-        <div>
-            
+        <div className="header">
+                <div className="logo-container">
+                     <img className="logo"
+                     src="https://img.freepik.com/premium-photo/anime-girl-eating-bowl-noodles-with-chopsticks-generative-ai_958124-30479.jpg?w=1060">
+                     </img>
+                </div>
+
+            <div className="nav-items" >
+                <ul >
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>🛒</li>
+                </ul>
+            </div>
+
         </div>
     )
+
+
+    const RestaurantCard = () =>{
+        return(
+            <div className="res-card">
+
+            </div>
+        )
+    } 
 }
+
+    const Body = ()=> {
+        return (
+            <div className="body">
+                <div className="Search">Search</div>
+                <div className="res-container">
+
+                </div>
+
+            </div>
+        )
+    }
 
 
 const AppLayout = ()=>{
     return(
 
         <div>
-            <h1>Hello from JS</h1>
+            <Header/>
+            <Body/>
         </div>
     )
 }
