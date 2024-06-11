09ca4e787d3bc5748636e000034d644719898fd5
AnkishSharma24 <142089148+AnkishSharma24@users.noreply.github.com>
2024-06-11 13:03:21 +0530

    Menu-Completed

--- src/component.js/Header.js
+++ src/component.js/Header.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { LOGO_URL } from "../../utils/constant";
+import { Link } from "react-router-dom";
+
 
 const Header = ()=>{
     let [btnName, setBtnName] = useState("Login");
@@ -17,9 +19,9 @@ const Header = ()=>{
 
             <div className="nav-items" >
                 <ul >
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home </Link></li>
+                   <li><Link to="/about"> About Us </Link></li>
+                    <li><Link to="/contact"> Contact Us </Link></li>
                     <li>🛒</li>
                     <li>
                              <button
